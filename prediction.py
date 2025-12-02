b4f2189ca9b1dc3f578eed6396c1c8297681bb09
lagilagipaldi <nafarevaldi94@gmail.com>
2025-12-02 13:23:59 +0700

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,12 +1,5 @@
-import pickle
-import numpy as np
-
-# load model
-with open("knn_model.sav", "rb") as f:
-    clf = pickle.load(f)
-
-labels = ["setosa", "versicolor", "virginica"]
+import joblib
 
 def predict(data):
-    pred_idx = clf.predict(data)[0]
-    return labels[pred_idx]
+    clf = joblib.load("rf_model.sav")
+    return clf.predict(data)
